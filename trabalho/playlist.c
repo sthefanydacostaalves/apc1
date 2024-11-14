3dc03e83db22cf4568213784caf1ff2808f6073b
Sthefany <sthefanycalves016@gmail.com>
2024-11-14 15:13:33 -0300

    trabalho correto

--- trabalho/playlist.c
+++ trabalho/playlist.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
-int main() {
+#include <stdlib.h>
 
 struct playlist_t{                            // usar do while :) 
     char musica [30];
     char estilo [30];
-
+   
 };
-struct playlist_t playlist;
+
+int main() {
+
+
+struct playlist_t playlist[30];
 int opcao=0;
+int j =0;
 
-
-// do {
-printf(" P L A Y L I S T ");
-printf("para adicionar musica digite.... 1");
-printf("para ver sua playlist digite... 2 ");
-printf("para sair digite...3");
+do {
+printf("--- P L A Y L I S T --- \n");
+printf("Sua playlist tem o total de %i musicas.\n", j);
+printf("para adicionar musica digite - 1\n");
+printf("para ver sua playlist digite - 2\n");
+printf("para sair digite - 3\n");
 printf("digite a sua opcao:  ");
 scanf("%i",&opcao);
-//   getchar();
-//           switch (opcao) {
-//             case 1: printf("digite o nome da musica: ");
-//                     scanf("%s",playlist.musica);
-//                     printf("digite o genero da musica:  ");
-//                     scanf("%s",playlist.estilo);        
-//             case 2 : printf()}
+  getchar();
+           switch (opcao) {
+             case 1: 
+             //printf("%i\n", j);
+                
+                 printf("digite o nome da musica: ");
+                     scanf("%s",playlist[j].musica);
+                     printf("digite o genero da musica:  ");
+                     scanf("%s",playlist[j].estilo);  
+                     
+                     j++;
+                     break;  
+
+             case 2: 
+             printf("--- S U A  P L A Y L I S T ---");
+             for (int i=0; i<j; i++){
+                    printf("\nmusica %d\n", i+1);
+                     printf("%s\n",playlist[i].musica);
+                     printf("%s\n",playlist[i].estilo);
+                    
+              } break;
+             
+
+             case 3: 
+             printf("ate logo!"); break;
+             
+             default:  printf("\nopcao invalida! aperte enter para voltar para o menu.");
+             printf("\n");
+             getchar ();}
+ 
+ }  while (opcao !=3); 
 
 
 
